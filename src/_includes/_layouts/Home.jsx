87a79bcccfcb75e2529c14d2a39cf0b48a1c78fb
Caposto <christiand.apostol@outlook.com>
2025-10-20 18:45:19 -0400

    added affiliate link

--- src/_includes/_layouts/Home.jsx
+++ src/_includes/_layouts/Home.jsx
@@ -29,6 +29,15 @@ export default function Home({ title, organizations, purpose, comp }) {
         <comp.OurPurpose purpose={purpose} />
         <comp.ExploreManual />
         <comp.AboutUs organizations={organizations} />
+        {/* Affiliate link for vot-er */}
+        <div className="flex justify-center pb-16">
+          <a
+            href="https://vot-er.org/act/?organizationId=e2e3e6f0-b933-4245-95d2-a3f69e4c633f&userId=537e60aa-e25b-47e5-b1f8-f5f68d566d36&ref=emdnj"
+            className="bg-primary hover:bg-secondary transition-colors duration-300 text-white text-md md:text-xl font-bold py-3 px-10 rounded-md"
+          >
+            Protect Your Care - Register to Vote
+          </a>
+        </div>
         <comp.Footer />
       </body>
     </html>
